1e380a7eb2cc44dd8e20af842d04c4d261005195
taha <rtdonuk@gmail.com>
2023-02-05 14:38:14 +0300

    open file feature implemented - opens the selected file in a new tab - sets tab title to selected file name

--- note-manager/src/main/java/org/manager/note/NoteManagerApp.java
+++ note-manager/src/main/java/org/manager/note/NoteManagerApp.java
@@ -11,7 +11,7 @@ public class NoteManagerApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(NoteManagerApp.class.getResource("main.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 1200, 800);
         stage.setTitle("Note Manager!");
         stage.setScene(scene);
         stage.show();
--- note-manager/src/main/java/org/manager/note/controls/MainController.java
+++ note-manager/src/main/java/org/manager/note/controls/MainController.java
@@ -4,8 +4,12 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.control.TextArea;
+import javafx.stage.FileChooser;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 
 public class MainController {
     @FXML
@@ -22,9 +26,34 @@ public class MainController {
 
     @FXML
     public void addNewTab() throws IOException {
-        Tab tab = (Tab) FXMLLoader.load(MainController.class.getResource("new-tab.fxml"));
+        tabs.getTabs().add(createTab("New Tab"));
+    }
 
-        tab.setText("new tab");
+    private Tab createTab(String title) throws IOException {
+        Tab tab = FXMLLoader.load(MainController.class.getResource("new-tab.fxml"));
+
+        tab.setText(title);
+
+        return tab;
+    }
+
+    @FXML
+    public void openFile() throws IOException {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select a file to open");
+        fileChooser.setInitialDirectory(new File((String) System.getProperties().get("user.home")));
+
+        File selectedFile = fileChooser.showOpenDialog(tabs.getScene().getWindow());
+
+        String content = Files.readString(selectedFile.toPath());
+
+        Tab tab = createTab(selectedFile.getName());
         tabs.getTabs().add(tab);
+
+        TextArea textArea = (TextArea) tab.getContent();
+
+        textArea.setText(content);
+
+        tabs.getSelectionModel().clearAndSelect(tabs.getTabs().indexOf(tab));
     }
 }
